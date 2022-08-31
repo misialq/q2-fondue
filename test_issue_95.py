dc2aaba7fc3c058c94fc50b2bb5b5f7ae1fc0856
Michal Ziemski <mziemski@ethz.ch>
2022-08-31 16:35:21 +0200

    clean up the test script

--- test_issue_95.py
+++ test_issue_95.py
@@ -12,6 +12,9 @@ from qiime2.plugins import fondue
 from qiime2 import Artifact
 
 
+LOG_LEVEL = 'TRACE'
+EMAIL = "mziemski@ethz.ch"
+
 def run_main():
     ids = Artifact.import_data(
         "NCBIAccessionIDs", "ids-small.tsv"
@@ -20,10 +23,10 @@ def run_main():
     print("Fetching sequences (1)...")
     seqs = fondue.methods.get_sequences(
         accession_ids=ids,
-        email="mziemski@ethz.ch",
+        email=EMAIL,
         n_jobs=7,
         n_fasterq_jobs=2,
-        log_level="TRACE",
+        log_level=LOG_LEVEL,
         mode="large",
     )
     del seqs
@@ -32,10 +35,10 @@ def run_main():
     print("Fetching sequences (2)...")
     seqs = fondue.methods.get_sequences(
         accession_ids=ids,
-        email="mziemski@ethz.ch",
+        email=EMAIL,
         n_jobs=9,
         n_fasterq_jobs=4,
-        log_level="TRACE",
+        log_level=LOG_LEVEL,
         mode="large",
     )
     del seqs
@@ -44,10 +47,10 @@ def run_main():
     print("Fetching sequences (3)...")
     seqs = fondue.methods.get_sequences(
         accession_ids=ids,
-        email="mziemski@ethz.ch",
+        email=EMAIL,
         n_jobs=7,
         n_fasterq_jobs=1,
-        log_level="TRACE",
+        log_level=LOG_LEVEL,
         mode="large",
     )
     del seqs
@@ -56,10 +59,10 @@ def run_main():
     print("Fetching sequences (4)...")
     seqs = fondue.methods.get_sequences(
         accession_ids=ids,
-        email="mziemski@ethz.ch",
+        email=EMAIL,
         n_jobs=6,
         n_fasterq_jobs=1,
-        log_level="TRACE",
+        log_level=LOG_LEVEL,
         mode="regular",
     )
 
